bda43461f924a074ff7a39ee379f1e5807e4102e
dklabdo <111941564+dklabdo@users.noreply.github.com>
2024-05-08 18:12:25 +0100

    v2.6

--- src/app/Sections/Hero.jsx
+++ src/app/Sections/Hero.jsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 import 'animate.css';
 function Hero() {
   return (
-    <div className="my-10 scale-95 flex xl:flex-row   md:px-8 2xl:px-24 xl:px-14   w-full flex-col-reverse  items-center justify-between  ">
+    <div className=" scale-95 flex xl:flex-row   md:px-8 2xl:px-24 xl:px-14   w-full flex-col-reverse  items-center justify-between  ">
      
-      <div className="animate__animated animate__backInLeft xl:w-[60%] w-full my-10 xl:mt-36 xs:px-1 px-8 flex flex-col gap-8  md:px-14">
+      <div className="animate__animated animate__backInLeft xl:w-[60%] w-full my-10 xl:mt-24 xs:px-1 px-8 flex flex-col gap-8  md:px-14">
         <h1 className="xl:text-nowrap text-center xl:text-start"><span className="text-secondary">Le sourir </span><span>
         <Image src="/image/smile.svg" className="inline w-10" width={80} height={80} />
         </span> <span className="text-secondary">de votre enfant </span> <br /> <span className="text-primary">c’est notre responsabilité</span> </h1>
